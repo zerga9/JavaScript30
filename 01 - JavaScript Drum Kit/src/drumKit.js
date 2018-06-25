7ef98c6649ffdbb6d400d7bc2de61fa353dc025b
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-25 12:33:33 +0100

    first day drumKit done

---
+++ 01 - JavaScript Drum Kit/src/drumKit.js
@@ -0,0 +1,19 @@
+// e for event. can see keycode at www.keycode.info
+   function playSound(e){
+     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+     console.log(audio);
+     if(!audio) return;//stop the function from running all together
+     audio.currentTime = 0;//rewind to start
+     audio.play();
+     key.classList.add('playing');
+   };
+
+   function removeTransition(e) {
+     if(e.propertyName !== 'transform') return;
+     this.classList.remove('playing');
+   }
+
+   const keys = document.querySelectorAll('.key');
+   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+   window.addEventListener('keydown', playSound);
